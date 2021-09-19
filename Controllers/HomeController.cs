78481005d1c19984aaab8ee780b10e9e89c7887a
isyankarcoder <gencvolkan@gmail.com>
2021-09-19 09:22:30 -0700

    ..

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspnetCoreSecurity1
+{
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Error()
+    {
+        return View();
+    }
+
+    [Authorize]
+    public IActionResult  Secret(){
+        return View();
+    }
+    
+
+    public IActionResult Authenticate(){
+        return  RedirectToAction("Index");
+
+    }
+     
+}
+}
--- Startup.cs
+++ Startup.cs
@@ -8,32 +8,29 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace AspnetCoreSecurity1
-{
-    public class Startup
-    {
+namespace AspnetCoreSecurity1 {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddAuthentication ("CookieAuth").AddCookie ("cookieauth", config => {
+                config.Cookie.Name = "Grandmas.Cookie";
+            });
+
+            services.AddControllersWithViews ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseRouting();
+            app.UseRouting ();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints (endpoints => {
+                endpoints.MapDefaultControllerRoute ();
             });
         }
     }
